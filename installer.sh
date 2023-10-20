40977a83804fdc735f48f8c5abbd9ad980e4eb14
nicloas.hornek <nicolas.hornek@bright-testing.de>
2023-10-20 15:11:34 +0200

    1

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 rm -r .config/nvim/custom
 stow .
