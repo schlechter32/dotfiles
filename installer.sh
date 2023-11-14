06bd50e56f27b00c0a62c5d97039c442b7d2d271
nh <nh@bt.de>
2023-11-14 11:15:45 +0100

    added nvchad conf

--- installer.sh
+++ installer.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 rm -r .config/nvim/custom
+brew install luarocks ripgrep stylua lazygit wezterm ccls tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 stow .
