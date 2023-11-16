51ccc0cf67e944ba99d7d37d2b1787fe80a839ea
nicloas.hornek <nicolas.hornek@bright-testing.de>
2023-11-16 10:10:51 +0100

    removed brew installer local

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@ mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar 
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 #git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 rm -r .config/nvim/custom
-brew install luarocks ripgrep stylua lazygit wezterm ccls tmux
+brew install luarocks ripgrep stylua lazygit ccls tmux
+brew install --cask wezterm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 stow .
