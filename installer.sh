8b6678176b0057a2cc3001b1c5c8b0c3770e4b55
nicloas.hornek <nicolas.hornek@bright-testing.de>
2023-11-16 09:59:58 +0100

    added brew installer

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 #git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 rm -r .config/nvim/custom
