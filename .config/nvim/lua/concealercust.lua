1cda2d38dbf50af70de628761a00cba79d108759
nhornek <nicolas.hornek@outlook.com>
2024-02-26 08:16:32 +0100

    obsidian-plugin

--- .config/nvim/lua/concealercust.lua
+++ .config/nvim/lua/concealercust.lua
@@ -2,3 +2,5 @@
 vim.opt.highlight.Conceal = { gui = "bold" }
 vim.cmd "highlight Conceal gui=bold"
 vim.cmd "highlight @markup.link.url cterm=underline gui=underline"
+
+vim.cmd "highlight @markup.heading.1.markdown cterm=bold gui=bold guifg=#e78284"
