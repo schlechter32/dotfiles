bf8f9733dd1d80ab226708e6b3a5c44b77cd350c
nhornek <nicolas.hornek@ikr.uni-stuttgart.de>
2024-03-03 20:38:18 +0100

    fixed lsp eslint

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -190,18 +190,18 @@ return {
           -- formatting.ocamlformat,
 
           -- diagnostics
-          diagnostics.eslint_d.with {
-            condition = function(utils)
-              return utils.root_has_file { ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }
-            end,
-          },
+          -- diagnostics.eslint_d.with {
+          --   condition = function(utils)
+          --     return utils.root_has_file { ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }
+          --   end,
+          -- },
 
           -- code actions
-          code_actions.eslint_d.with {
-            condition = function(utils)
-              return utils.root_has_file { ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }
-            end,
-          },
+          -- code_actions.eslint_d.with {
+          --   condition = function(utils)
+          --     return utils.root_has_file { ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }
+          --   end,
+          -- },
         },
       }
 
