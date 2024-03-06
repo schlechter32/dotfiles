42577bbecd5a62a644320ab5b3166841d45aaf2f
nclshrnk <nicolas.hornek@ikr.uni-stuttgart.de>
2024-03-06 11:28:58 +0100

    removed cust concealing fixed inline markdown

--- .config/nvim/lua/concealercust.lua
+++ .config/nvim/lua/concealercust.lua
@@ -1,6 +1,6 @@
 -- print "setting concealer"
-vim.opt.highlight.Conceal = { gui = "bold" }
-vim.cmd "highlight Conceal gui=bold"
-vim.cmd "highlight @markup.link.url cterm=underline gui=underline"
+-- vim.opt.highlight.Conceal = { gui = "bold" }
+-- vim.cmd "highlight Conceal gui=bold"
+-- vim.cmd "highlight @markup.link.url cterm=underline gui=underline"
 
-vim.cmd "highlight @markup.heading.1.markdown cterm=bold gui=bold guifg=#e78284"
+-- vim.cmd "highlight @markup.heading.1.markdown cterm=bold gui=bold guifg=#e78284"
