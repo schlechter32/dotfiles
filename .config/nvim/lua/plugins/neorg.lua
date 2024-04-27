03e4013fe8b3fe5b1569c7a93ae190efb26f32b8
nicloas.hornek <nicolas.hornek@bright-testing.de>
2024-04-27 14:27:16 +0200

    fixed neovim and added new ssh scripts

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,33 +1,41 @@
 return {
-  "nvim-neorg/neorg",
-  -- build = ":Neorg sync-parsers",
-  -- tag = "*",
-  lazy = false,
-  ft = "norg",
-  dependencies = { "luarocks.nvim" },
+  {
 
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.summary"] = {}, -- Generate summaryGenerate summary)
-        -- can be enabled when nvim v10 is used
-        -- ["core.temputempus"] = {}, -- Generate summaryGenerate summary)
-        -- ["core.ui.calendar"] = {},-- Show calendar
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+    "nvim-neorg/neorg",
+    -- build = ":Neorg sync-parsers",
+    -- tag = "*",
+    lazy = false,
+    ft = "norg",
+    dependencies = { "luarocks.nvim" },
 
-        ["core.export"] = {}, -- Loads default behaviour
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              secondBrain = "~/secondBrain/",
-              -- areas = "~/phd/papers",
-              -- reso = "~/know",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.summary"] = {}, -- Generate summaryGenerate summary)
+          -- can be enabled when nvim v10 is used
+          -- ["core.temputempus"] = {}, -- Generate summaryGenerate summary)
+          -- ["core.ui.calendar"] = {},-- Show calendar
+
+          ["core.export"] = {}, -- Loads default behaviour
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                secondBrain = "~/secondBrain/",
+                -- areas = "~/phd/papers",
+                -- reso = "~/know",
+              },
+              default_workspace = secondBrain,
             },
-            default_workspace = secondBrain,
           },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
 }
