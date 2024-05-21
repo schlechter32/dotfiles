46db481be5390d69948dd67a3b3f70b16ba6fc90
nclshrnk <nicolas.hornek@ikr.uni-stuttgart.de>
2024-05-21 12:28:49 +0200

    added toggle for format on save

--- .config/nvim/lua/user/format_on_save.lua
+++ .config/nvim/lua/user/format_on_save.lua
@@ -1,9 +1,15 @@
+vim.g.format_on_save = 1
+
+vim.cmd [[
+  command! EnableFormatOnSave lua vim.g.format_on_save = 1
+  command! DisableFormatOnSave lua vim.g.format_on_save = 0
+]]
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("format_on_save", { clear = true }),
   pattern = "*",
   desc = "Run LSP formatting on a file on save",
   callback = function()
-    if vim.fn.exists ":Format" > 0 then
+    if vim.fn.exists ":Format" > 0 and vim.g.format_on_save == 1 then
       vim.cmd.Format()
     end
   end,
