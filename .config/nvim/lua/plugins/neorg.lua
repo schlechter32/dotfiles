5a647c1006c5cfb15fceaa5f7b2fe48f3048946c
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2024-08-07 10:16:19 +0200

    remove neorg and tsx

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,41 +1,42 @@
-return {
-  {
-
-    "vhyrro/luarocks.nvim",
-    priority = 1000, -- We'd like this plugin to load first out of the rest
-    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-  },
-  {
-    "nvim-neorg/neorg",
-    -- build = ":Neorg sync-parsers",
-    -- tag = "*",
-    lazy = false,
-    ft = "norg",
-    dependencies = { "luarocks.nvim" },
-
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.summary"] = {}, -- Generate summaryGenerate summary)
-          -- can be enabled when nvim v10 is used
-          -- ["core.temputempus"] = {}, -- Generate summaryGenerate summary)
-          -- ["core.ui.calendar"] = {},-- Show calendar
-
-          ["core.export"] = {}, -- Loads default behaviour
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                secondBrain = "~/secondBrain/",
-                -- areas = "~/phd/papers",
-                -- reso = "~/know",
-              },
-              default_workspace = secondBrain,
-            },
-          },
-        },
-      }
-    end,
-  },
-}
+-- return {
+--   {
+--
+--     "vhyrro/luarocks.nvim",
+--     priority = 1000, -- We'd like this plugin to load first out of the rest
+--     config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+--   },
+--   {
+--     "nvim-neorg/neorg",
+--     -- build = ":Neorg sync-parsers",
+--     -- tag = "*",
+--     lazy = false,
+--     ft = "norg",
+--     dependencies = { "luarocks.nvim" },
+--
+--     config = function()
+--       require("neorg").setup {
+--         load = {
+--           ["core.defaults"] = {}, -- Loads default behaviour
+--           ["core.concealer"] = {}, -- Adds pretty icons to your documents
+--           ["core.summary"] = {}, -- Generate summaryGenerate summary)
+--           -- can be enabled when nvim v10 is used
+--           -- ["core.temputempus"] = {}, -- Generate summaryGenerate summary)
+--           -- ["core.ui.calendar"] = {},-- Show calendar
+--
+--           ["core.export"] = {}, -- Loads default behaviour
+--           ["core.dirman"] = { -- Manages Neorg workspaces
+--             config = {
+--               workspaces = {
+--                 secondBrain = "~/secondBrain/",
+--                 -- areas = "~/phd/papers",
+--                 -- reso = "~/know",
+--               },
+--               default_workspace = secondBrain,
+--             },
+--           },
+--         },
+--       }
+--     end,
+--   },
+-- }
+return {}
