be8133459be3e463dcfce491b2ab8f62b1f5793c
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2025-02-20 14:33:51 +0100

    improve linkconf

--- linkconf.sh
+++ linkconf.sh
@@ -17,8 +17,9 @@ ln -sf $REPODIR/.config/wezterm/* ~/.config/wezterm
 ln -sf $REPODIR/.config/alacritty/* ~/.config/alacritty
 ln -sf $REPODIR/.config/starship.toml ~/.config/
 ln -sf $REPODIR/.zshrc $HOME
+ln -sf $REPODIR/nbin/ $HOME
 ln -sf $REPODIR/.local/share/fonts/* ~/.local/share/fonts
-
+ln -sf $REPODIR/.gitconfig $HOME
 
 if command -v nix &>/dev/null && nix --version &>/dev/null; then
     echo "On nix assuming shelltools already installed"
@@ -28,9 +29,14 @@ else
 
     if [ $(uname) == "Darwin" ]; then
 
-        ln -sf $REPODIR/.gitconfig $HOME
     else
-        ln -sf $REPODIR/.gitconfigikr $/HOME/.gitconfig
+
+if [[ $(hostname) == *"cnode"* ]]; then
+        ln -sf $REPODIR/.gitconfigikr $HOME/.gitconfig
+elif [[ $(hostname) == *"pc"* ]]; then
+
+        ln -sf $REPODIR/.gitconfigikr $HOME/.gitconfig
+fi
     fi
 fi
 
