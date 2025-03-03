609223b3ef7415af74e28bfff1879e0455f32924
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2025-03-03 12:02:01 +0100

    st uv wrapper

--- zsh/ikrhosts.zsh
+++ zsh/ikrhosts.zsh
@@ -17,7 +17,15 @@ export ST_PATH=/u/home/wima/nclshrnk/source/SimTree_wrapper
 function st(){
     source /u/home/wima/nclshrnk/source/SimTree_wrapper/st_wrapper.bash
 }
+
+elif command -v nix &>/dev/null && nix --version &>/dev/null; then
+
+export ST_PATH=/home/nclshrnk/source/simtree_wrapper
+function st(){
+    source /home/nclshrnk/source/simtree_wrapper/st_uv_wrapper.bash
+}
 elif [[ $(hostname) == *"cobra"* ]]; then
+
 
 # export JULIAUP_DEPOT_PATH="$LAB_BULK_HOME/.julia/"
 # export JULIA_DEPOT_PATH="$LAB_BULK_HOME/.julia/"
