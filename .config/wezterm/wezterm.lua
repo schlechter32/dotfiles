6ddad8e33a54ae5e885db716755821cbac688138
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2025-03-11 16:30:47 +0100

    new ingress io

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -104,7 +104,7 @@ config.font = wezterm.font_with_fallback({
 	{ family = "CaskaydiaCove Nerd Font", scale = 1.2 },
 	{ family = "FantasqueSansM Nerd Font", scale = 1.2 },
 })
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 40
 local homedir = os.getenv("HOME")
 config.window_background_image = homedir .. "/.config/wezterm/AutumnalPeachMatt.jpg"
