fd1659ceaf0848fc0325c88c458145dc80f4277f
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2025-04-25 10:41:37 +0200

    tweaked animation speed

--- .config/nvim/lua/plugins/vim_maximizer.lua
+++ .config/nvim/lua/plugins/vim_maximizer.lua
@@ -11,8 +11,13 @@ return {
   },
   config = function()
     vim.o.winwidth = 5
-    vim.o.winminwidth =5 
+    vim.o.winminwidth = 5
     vim.o.equalalways = false
-    require("windows").setup()
+    require("windows").setup {
+      animation = {
+        duration = 50,
+        fps = 60,
+      },
+    }
   end,
 }
