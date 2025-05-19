182c7cc31940f3143025d02872f31a4871dfc382
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2025-05-19 10:56:59 +0200

    removed pkg offline since we have internet again

--- zsh/ikrhosts.zsh
+++ zsh/ikrhosts.zsh
@@ -1,18 +1,23 @@
 BULK_HOME="/u/bulk/home/wima/$USER"
 LAB_BULK_HOME="/home/bulk"
 
-export PATH="$HOME/.juliaup/bin/:$PATH"
+# export PATH="$HOME/.juliaup/bin/:$PATH"
 if [[ $(hostname) == *"cnode"* ]]; then
 
-export JULIAUP_DEPOT_PATH="$BULK_HOME/julia/"
-export JULIA_DEPOT_PATH="$BULK_HOME/julia/"
+export JULIAUP_DEPOT_PATH="$BULK_HOME/.julia/"
+export JULIA_DEPOT_PATH="$BULK_HOME/.julia/"
+#
+export PATH="$BULK_HOME/.juliaup/bin/:$PATH"
+
+# export PATH="/u/bulk/home/wima/nclshrnk/julia/juliaup/julia-1.11.3/bin/:$PATH"
+#
 #
 # export PATH="$BULK_HOME/.juliaup/bin/:$PATH"
 # export PYENV_ROOT="$BULK_HOME/.pyenv"
 
-export JULIAUP_DEPOT_PATH="$HOME/.julia/"
-export JULIA_DEPOT_PATH="$HOME/.julia/"
-export JULIA_PKG_OFFLINE="true"
+# export JULIAUP_DEPOT_PATH="$HOME/.julia/"
+# export JULIA_DEPOT_PATH="$HOME/.julia/"
+# export JULIA_PKG_OFFLINE="true"
 export CPLEX_STUDIO_BINARIES="/ext/cplex/cplex/bin/x86-64_linux"
 export ST_PATH=/u/home/wima/nclshrnk/source/SimTree_wrapper
 function st(){
