ae23892c11a5980117c0b72243c3556623ef8b18
nclshrnk <nicolas.hornek@ikr.uni-stutgart.de>
2026-02-13 14:36:45 +0100

    move to netlab bulk julia

--- zsh/ikrhosts.zsh
+++ zsh/ikrhosts.zsh
@@ -37,9 +37,11 @@ function st(){
 }
 
 elif command -v nix &>/dev/null && nix --version &>/dev/null; then
-export nethome=/bulk/netserv0/wimas/nclshrnk/
-export JULIAUP_DEPOT_PATH="$HOME/.julia/"
-export JULIA_DEPOT_PATH="$HOME/.julia/"
+# export nethome=/bulk/netserv0/wimas/nclshrnk/
+# export JULIAUP_DEPOT_PATH="$HOME/.julia/"
+# export JULIA_DEPOT_PATH="$HOME/.julia/"
+export PATH="$BULK_HOME/.juliaup/bin/:$PATH"
+export PATH="$BULK_HOME/.julia/bin/:$PATH"
 export PATH="$HOME/.julia/bin/:$PATH"
 export UV_CACHE_DIR="$LAB_BULK_HOME/.uv/cache"
 export ST_PATH=/home/nclshrnk/source/simtree_wrapper
